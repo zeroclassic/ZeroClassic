d69da93c852b11780d8251a4d737ae98861d47a4
miodragpop <miodragpopovic@stcable.net>
2021-03-22 15:33:50 +0100

    rust packages: use vendored-sources archive instead of registry

--- depends/packages/packages.mk
+++ depends/packages/packages.mk
@@ -1,5 +1,5 @@
 zcash_packages := libsodium utfcpp
-packages := boost libevent zeromq $(zcash_packages) googletest
+packages := boost libevent zeromq $(zcash_packages) vendored_crates googletest
 native_packages := native_clang native_ccache native_rust
 
 wallet_packages=bdb
---
+++ depends/packages/vendored_crates.mk
@@ -0,0 +1,6 @@
+package=vendored_crates
+$(package)_version=v4.3.0
+$(package)_download_path=https://github.com/miodragpop/zeroclassic-vendored-crates/releases/download/$($(package)_version)
+$(package)_file_name=vendored-crates.tar.gz
+$(package)_download_file=vendored-sources-$($(package)_version).tar.gz
+$(package)_sha256_hash=92ea9406b30040adc784c7e9340adb782f85d0f301db1f90db609844cfde4cb9
