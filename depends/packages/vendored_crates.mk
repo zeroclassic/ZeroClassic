488816e36961ba9c5c2014b39111b94047f2b482
miodragpop <miodragpopovic@stcable.net>
2021-03-30 10:09:53 +0200

    vendored crates updated

--- depends/packages/vendored_crates.mk
+++ depends/packages/vendored_crates.mk
@@ -1,6 +1,6 @@
 package=vendored_crates
-$(package)_version=v4.3.0
+$(package)_version=v4.3.1
 $(package)_download_path=https://github.com/miodragpop/zeroclassic-vendored-crates/releases/download/$($(package)_version)
 $(package)_file_name=vendored-crates.tar.gz
 $(package)_download_file=vendored-sources-$($(package)_version).tar.gz
-$(package)_sha256_hash=92ea9406b30040adc784c7e9340adb782f85d0f301db1f90db609844cfde4cb9
+$(package)_sha256_hash=78a50da3b0068628efbbdf4ee47929f96a0b6cbd49459f149d813f18859a42d9
