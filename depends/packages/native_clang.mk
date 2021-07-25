4b06012969df8920ffcedcea8f53ecec6ac076ca
Miodrag Popović <miodragpopovic@stcable.net>
2021-07-25 10:55:46 +0200

    clang 12.0.1

--- depends/packages/native_clang.mk
+++ depends/packages/native_clang.mk
@@ -1,11 +1,11 @@
 package=native_clang
 $(package)_major_version=12
-$(package)_version=12.0.0
+$(package)_version=12.0.1
 $(package)_download_path=https://github.com/llvm/llvm-project/releases/download/llvmorg-$($(package)_version)
 $(package)_download_path_linux=https://github.com/llvm/llvm-project/releases/download/llvmorg-$($(package)_version)
 $(package)_download_file_linux=clang+llvm-$($(package)_version)-x86_64-linux-gnu-ubuntu-16.04.tar.xz
 $(package)_file_name_linux=clang-llvm-$($(package)_version)-x86_64-linux-gnu-ubuntu-16.04.tar.xz
-$(package)_sha256_hash_linux=9694f4df031c614dbe59b8431f94c68631971ad44173eecc1ea1a9e8ee27b2a3
+$(package)_sha256_hash_linux=6b3cc55d3ef413be79785c4dc02828ab3bd6b887872b143e3091692fc6acefe7
 $(package)_download_path_darwin=https://github.com/llvm/llvm-project/releases/download/llvmorg-12.0.0
 $(package)_download_file_darwin=clang+llvm-12.0.0-x86_64-apple-darwin.tar.xz
 $(package)_file_name_darwin=clang-llvm-12.0.0-x86_64-apple-darwin.tar.xz
@@ -13,7 +13,7 @@ $(package)_sha256_hash_darwin=7bc2259bf75c003f644882460fc8e844ddb23b27236fe43a27
 $(package)_download_path_freebsd=https://github.com/llvm/llvm-project/releases/download/llvmorg-$($(package)_version)
 $(package)_download_file_freebsd=clang+llvm-$($(package)_version)-amd64-unknown-freebsd11.tar.xz
 $(package)_file_name_freebsd=clang-llvm-$($(package)_version)-amd64-unknown-freebsd11.tar.xz
-$(package)_sha256_hash_freebsd=8ff2ae0863d4cbe88ace6cbcce64a1a6c9a8f1237f635125a5d580b2639bba61
+$(package)_sha256_hash_freebsd=38857da36489880b0504ae7142b74abe41cf18711a6bb25ca96792d8190e8b0e
 
 # Ensure we have clang native to the builder, not the target host
 ifneq ($(canonical_host),$(build))
