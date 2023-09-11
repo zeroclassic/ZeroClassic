52097749e94837d4d5a0dbd3c5ccae7e115924dd
Miodrag Popović <miodragpopovic@stcable.net>
2023-09-11 09:23:49 +0200

    Fix for crossbuild to windows host

--- depends/hosts/mingw32.mk
+++ depends/hosts/mingw32.mk
@@ -3,7 +3,9 @@ mingw32_CXXFLAGS=$(mingw32_CFLAGS)
 
 ifneq ($(ZERC_TOOLCHAIN), GCC)
   mingw32_CXXFLAGS += -isystem $(host_prefix)/include/c++/v1
+  mingw32_CPPFLAGS=-D_LIBCXXABI_DISABLE_VISIBILITY_ANNOTATIONS
   mingw32_LDFLAGS?=-fuse-ld=lld
+  mingw32_LDFLAGS+=-L/usr/lib/gcc/x86_64-w64-mingw32/$(shell x86_64-w64-mingw32-g++-posix -dumpversion)
 else
   mingw32_CC = $(host_toolchain)gcc-posix
   mingw32_CXX = $(host_toolchain)g++-posix
